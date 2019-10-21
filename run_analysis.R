e7393742c970888065ba596dbd9fa2e91a7b48ee
zhounanvet <49698756+zhounanvet@users.noreply.github.com>
2019-10-20 19:23:55 -0700

    update the code

--- run_analysis.R
+++ run_analysis.R
@@ -14,48 +14,37 @@ activity<-read.table('./get_clean_data/UCI_HAR_Dataset/activity_labels.txt')
 features<-read.table('./get_clean_data/UCI_HAR_Dataset/features.txt')
 
 #####1.Merges the training and the test sets to create one data set########
-all<- rbind(train,test)
+subject <- rbind(subject_train,subject_test)
+label<- rbind(label_train,label_test)
+measures<- rbind(train, test)
+colnames(subject)<-'subject_id'
+colnames(label)<- 'activity'
+all<- cbind(measures,subject, label)
+
 
 #####Extracts only the measurements on the mean and standard deviation for each measurement.####
-indx_mean<-grep('mean()',features$V2)
-indx_std<-grep('std()',features$V2)
-
-meanstd<-all[,c(indx_mean,indx_std)]
-
+indx<-grep('(mean|std)\\(\\)',features$V2)
+featureselected <- as.character(features[indx,'V2'])
+selected<- all[,c(indx, 562,563)]
+colnames(selected)<- c(featureselected,'subject_id','activity')
 
 ######Uses descriptive activity names to name the activities in the data set######
-labelt<-c(label_train$V1)
-for (i in 1:6){
-  labelt<-gsub(activity$V1[i],activity$V2[i],labelt)
-}
-
-labeltest<-c(label_test$V1)
-for (i in 1:6){
-  labeltest<-gsub(activity$V1[i],activity$V2[i],labeltest)
-}
-
-label<-c(labelt,labeltest)
-
-all_with_label<-cbind(all,label)
-
+selected_with_label<-merge(selected,activity,by.x = 'activity', by.y='V1')
+selected_with_label<- selected_with_label[,!names(selected_with_label)=='activity']
+names(selected_with_label)[68]<-'activity'
 
 #######Appropriately labels the data set with descriptive variable names.######
-names(all)<-features$V2
-all_with_name<-cbind(all,label)
-
+selected_with_label
 
 
 ##### creates a second, independent tidy data set with the #######
 #####average of each variable for each activity and each subject.#######
-subject<-c(subject_train$V1, subject_test$V1)
-
-all_with_subject <- cbind(subject, all_with_label)
-
-name<-names(all_with_subject)[c(-1,-563)]
-
 library(dplyr)
 
-final<-all_with_subject %>% group_by(subject,label) %>% 
-                     summarise_if(is.numeric,mean,na.rm = TRUE)
+final<-selected_with_label %>% 
+         group_by(subject_id,activity) %>% 
+            summarise_if(is.numeric,mean,na.rm = TRUE)
 
-write.table(final, file='./get_clean_data/clean_data.txt',row.names = FALSE)
+write.table(final, file='./get_clean_data/clean_data.txt',row.name = FALSE )
+
+
